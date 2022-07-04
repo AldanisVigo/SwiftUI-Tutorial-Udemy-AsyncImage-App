d936e61e21d14d06e12907ac7eaa80ff1b28a098
Aldanis Vigo <aldanisvigo@gmail.com>
2022-07-03 20:01:10 -0500

    Initial Commit

---
+++ Shared/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  Shared
+//
+//  Created by Aldanis Vigo on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
